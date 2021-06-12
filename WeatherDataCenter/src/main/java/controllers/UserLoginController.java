9b597d72402884a8b2dd698fa5ff041fbf2e8fcd
Tim James <32233010+tjames222@users.noreply.github.com>
2021-06-11 18:56:20 -0700

    class path fix

--- WeatherDataCenter/src/main/java/controllers/UserLoginController.java
+++ WeatherDataCenter/src/main/java/controllers/UserLoginController.java
@@ -3,6 +3,7 @@ package controllers;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+@SuppressWarnings("deprecation")
 @ManagedBean
 @ViewScoped
 public class UserLoginController {
